690740fd492d75a7eba8c0ab568320deda7bbb4e
ViniciusOLucio <vinicius.olucio@gmail.com>
2024-12-12 11:08:16 -0300

    setando zap

---
+++ bot.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+import requests
+
+dir_path = os.getcwd()
+chrome_options2 = Options()
+chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/pasta/sessao")
+driver = webdriver.Chrome(options=chrome_options2)
+driver.get('https://web.whatsapp.com/')
+time.sleep(120)
+#API 
+agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
+#CHAVE    HirTsOyzmpHLyAh7DbPKWT5RgWf7N8jb
+api = requests.get("https://editacodigo.com.br/index/api-whatsapp/HirTsOyzmpHLyAh7DbPKWT5RgWf7N8jb" ,  headers=agent)
+time.sleep(1)
+api = api.text
+api = api.split(".n.")
+bolinha_notificacao = api[3].strip()
+contato_cliente = api[4].strip()
+caixa_msg = api[5].strip()
+msg_cliente = api[6].strip()
+caixa_msg2 = api[7].strip()
+caixa_pesquisa = api[8].strip()
+
+def bot():
+    try:
+        bolinha = driver.find_element(By.CLASS_NAME, bolinha_notificacao)
+        bolinha = driver.find_elements(By.CLASS_NAME, bolinha_notificacao)
+        clica_bolinha = bolinha[-1]
+        acao_bolinha = webdriver.common.action_chains.ActionChains(driver)
+        acao_bolinha.move_to_element_with_offset(clica_bolinha, 0, -20)
+        acao_bolinha.click()
+        acao_bolinha.perform()
+        acao_bolinha.click()
+        acao_bolinha.perform()
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
